fc12a0efd9bc40e50c4f155939467b06255f9b31
eliran <manzeli@me.com>
2020-08-04 14:20:48 +0300

    Updates

---
+++ precache-manifest.17bb0497fcd1f8312e9f59084e38ecf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8e3f05506d4f8371a3388e9805a0b88",
+    "url": "/monsters-relodex/index.html"
+  },
+  {
+    "revision": "5a9be907cd09d365eb6c",
+    "url": "/monsters-relodex/static/css/main.7b3ddf54.chunk.css"
+  },
+  {
+    "revision": "d064ea85da7c2e271b4d",
+    "url": "/monsters-relodex/static/js/2.d9d4b753.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-relodex/static/js/2.d9d4b753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a9be907cd09d365eb6c",
+    "url": "/monsters-relodex/static/js/main.f5fb07e5.chunk.js"
+  },
+  {
+    "revision": "c3ba1f1cd420938ccc03",
+    "url": "/monsters-relodex/static/js/runtime-main.92953fd5.js"
+  }
+]);
